525cb2636bb487f525eb19f0802e5e903d529627
qh73xe <qh73xe@localhost.localdomain>
2018-10-03 00:56:11 +0900

    校正ツール redpen の導入用スクリプトを追加 ;

--- redpen/install.sh
+++ redpen/install.sh
@@ -1,1 +1,5 @@
-URL='https://github.com/redpen-cc/redpen/releases/download/redpen-1.10.0/redpen-1.10.0.tar.gz'
+VERSION=1.10.0
+URL=https://github.com/redpen-cc/redpen/releases/download/redpen-$VERSION/redpen-$VERSION.tar.gz
+wget $URL -P /tmp
+tar xvf "/tmp/redpen-$VERSION.tar.gz" -C "$HOME/.local/share/"
+ln -s "$HOME/.local/share/redpen-distribution-$VERSION/bin/redpen" "$HOME/.local/bin/redpen"
